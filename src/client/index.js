fa888c9d9433c146a127311d1ec8550a62a58873
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-09-01 00:05:54 +0100

    commit initial changes

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 // Import main Js Funtion
 import { init } from './js/app';
-
+import 'bootstrap';
 
 
 // Import main css
@@ -8,11 +8,7 @@ import './styles/entry.scss';
 
 // Inport images
 import './media/sun.svg';
-import './media/aircraft.svg';
-import './media/calendar.svg';
-import './media/time.svg';
-import './media/25.svg';
-import './media/background1.jpg'
+
 
 
 window.addEventListener("DOMContentLoaded", init);
