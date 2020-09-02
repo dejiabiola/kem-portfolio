10ae9b5213bcc530347323fe0b038855452c906f
Adedeji Abiola <adedejiabiola56@gmail.com>
2020-09-02 03:21:50 +0100

    commit changes

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,5 +2,15 @@
 
 export const init = function() {
 
-  
+  const closeNav = document.querySelector('.close-btn')
+  const openNav = document.querySelector('.open-nav') 
+  const mobileNav = document.querySelector('.mobile-nav')
+
+  closeNav.addEventListener('click', () => {
+    mobileNav.classList.remove('open')
+  })
+
+  openNav.addEventListener('click', () => {
+   mobileNav.classList.add('open')
+  })
 }
